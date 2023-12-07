3e8d560180d7fac490dcb4f9100d39b12e32abdf
Zhaojie Yin <55565844+BBjie@users.noreply.github.com>
2023-12-06 23:15:31 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+configs={
+    "max_new_tokens": 350,             # Maximum number of new tokens to generate
+    "top_p": 0.8,                      # sampling probability threshold
+    "temperature": 0.5,                # Controls randomness in generation
+    # "presence_penalty": 1.0,           # Penalty for repeating content that is already present
+    # "frequency_penalty": 1.0,          # Penalty for repeating the same line of thought
+    # "logprobs": 20,                    # Returns the top 20 token probabilities
+    # "echo": False                       # Includes the input prompt in the output
+}
